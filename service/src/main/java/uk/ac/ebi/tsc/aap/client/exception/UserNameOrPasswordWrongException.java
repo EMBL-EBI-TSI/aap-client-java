dac0f1bb630ea3ce6c7ec51168013ba65bbeed37
Upendra Kumbham <upendra.kumbham@gmail.com>
2017-09-25 16:14:59 +0100

    Add Custom Exception

---
+++ service/src/main/java/uk/ac/ebi/tsc/aap/client/exception/UserNameOrPasswordWrongException.java
@@ -0,0 +1,11 @@
+package uk.ac.ebi.tsc.aap.client.exception;
+
+/**
+ * Created by ukumbham on 25/09/2017.
+ * Responsible to throw custom error message to the user
+ */
+public class UserNameOrPasswordWrongException extends RuntimeException {
+    public UserNameOrPasswordWrongException(String message){
+        super(message);
+    }
+}
