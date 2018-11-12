c874f63599711def3223671b60e3da92c58cf652
Upendra Kumbham <upendra.kumbham@gmail.com>
2018-11-12 15:29:28 +0000

    Configure ObjectMapper to ignore unknown error attributes
    -Below are the changes we did as part of configuration:
    1. Configured mapper to ignore unrecognized error fields
    2. Add null and empty error response check
    3. If error response is null or empty throw AAP exception
    4. Extract status code from client http response

--- service/src/main/java/uk/ac/ebi/tsc/aap/client/repo/AAPResponseErrorHandler.java
+++ service/src/main/java/uk/ac/ebi/tsc/aap/client/repo/AAPResponseErrorHandler.java
@@ -1,26 +1,24 @@
 package uk.ac.ebi.tsc.aap.client.repo;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.ResponseErrorHandler;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
 import uk.ac.ebi.tsc.aap.client.exception.AAPException;
 import uk.ac.ebi.tsc.aap.client.exception.InvalidJWTTokenException;
 import uk.ac.ebi.tsc.aap.client.exception.TokenExpiredException;
 import uk.ac.ebi.tsc.aap.client.exception.TokenNotSuppliedException;
 import uk.ac.ebi.tsc.aap.client.model.ErrorResponse;
 import uk.ac.ebi.tsc.aap.client.util.RestUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 @Component
 public class AAPResponseErrorHandler implements ResponseErrorHandler {
@@ -32,10 +30,16 @@ public class AAPResponseErrorHandler implements ResponseErrorHandler {
         LOGGER.error("Error code : {}", response.getStatusCode());
         String result = new BufferedReader(new InputStreamReader(response.getBody()))
                 .lines().collect(Collectors.joining("\n"));
-        LOGGER.error("Error response : {}",result);
-        ObjectMapper mapper = new ObjectMapper();
-        ErrorResponse errorResponse = mapper.readValue(result,ErrorResponse.class);
-        throwException(errorResponse);
+        LOGGER.error("Error response : {}", result);
+        ErrorResponse errorResponse = null;
+        if (result != null && !result.isEmpty()) {
+            ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            errorResponse = mapper.readValue(result, ErrorResponse.class);
+            throwException(errorResponse, response);
+        } else {
+            throw new AAPException(null, response.getStatusCode());
+        }
+
 
     }
 
@@ -44,21 +48,21 @@ public class AAPResponseErrorHandler implements ResponseErrorHandler {
         return RestUtil.isError(response.getStatusCode());
     }
 
-    private void throwException(ErrorResponse errorResponse){
+    private void throwException(ErrorResponse errorResponse, ClientHttpResponse response) throws IOException {
         String error = errorResponse.getError();
         String message = errorResponse.getMessage();
-        if(errorResponse.getStatus()==HttpStatus.UNAUTHORIZED.value()){
-            switch (error){
-                case "INVALID_JWT" :
+        if (response.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+            switch (error) {
+                case "INVALID_JWT":
                     throw new InvalidJWTTokenException(message);
-                case "TOKEN_EXPIRED" :
+                case "TOKEN_EXPIRED":
                     throw new TokenExpiredException(message);
-                case "NO_TOKEN" :
+                case "NO_TOKEN":
                     throw new TokenNotSuppliedException(message);
-                default :
+                default:
                     throw new InvalidJWTTokenException(message);
             }
         }
-        throw new AAPException(errorResponse.getMessage(),errorResponse.getStatus());
+        throw new AAPException(errorResponse.getMessage(), response.getStatusCode());
     }
 }
